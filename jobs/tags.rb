5cb6506b0204f69f17d787e871fdf3ef5eb7339b
Jean-Nicholas Hould <nick.hould@gmail.com>
2013-08-17 15:54:50 -0400

    add mixpanel class

--- jobs/tags.rb
+++ jobs/tags.rb
@@ -1,6 +1,9 @@
 buzzwords = ['Paradigm shift', 'Leverage', 'Pivoting', 'Turn-key', 'Streamlininess', 'Exit strategy', 'Synergy', 'Enterprise', 'Web 2.0']
 buzzword_counts = Hash.new({ value: 0 })
 
+m = MixpanelAPI.new()
+m.funnel_list
+
 SCHEDULER.every '2s' do
   random_buzzword = buzzwords.sample
   buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
